b8e765b3c3e508a9df570b479f725611ab608a68
Pratik Raj <rajpratik71@gmail.com>
2019-07-29 14:49:07 +0530

    added script to automate process
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

--- exportManPages.sh
+++ exportManPages.sh
@@ -1,1 +1,15 @@
 for i in `ls /sbin`; do man /sbin/$i | > "$i".md; done
+
+for i in `ls /bin`; do man /bin/$i | > "$i".md; done
+
+
+for i in `ls /usr/bin`; do man /usr/bin/$i | > "$i".md; done
+
+
+for i in `ls /usr/sbin`; do man /usr/sbin/$i | > "$i".md; done
+
+
+for i in `ls /usr/local/sbin`; do man /usr/local/sbin/$i | > "$i".md; done
+
+
+for i in `ls /usr/local/bin`; do man /usr/local/bin/$i | > "$i".md; done
