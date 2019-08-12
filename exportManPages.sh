a75bdf33584920324d61322095114ba3eb3705e5
Pratik Raj <Rajpratik71@gmail.com>
2019-08-12 17:45:40 +0530

    Update exportManPages.sh

--- exportManPages.sh
+++ exportManPages.sh
@@ -1,11 +1,11 @@
-for i in `ls /sbin`; do man $i | > "$i".md; done
+for i in `ls /sbin`; do man $i > "$i".md; done
 
-for i in `ls /bin`; do man $i | > "$i".md; done
+for i in `ls /bin`; do man $i > "$i".md; done
 
-for i in `ls /usr/bin`; do man $i | > "$i".md; done
+for i in `ls /usr/bin`; do man $i > "$i".md; done
 
-for i in `ls /usr/sbin`; do man $i | > "$i".md; done
+for i in `ls /usr/sbin`; do man $i > "$i".md; done
 
-for i in `ls /usr/local/sbin`; do man $i | > "$i".md; done
+for i in `ls /usr/local/sbin`; do man $i > "$i".md; done
 
-for i in `ls /usr/local/bin`; do man $i | > "$i".md; done
+for i in `ls /usr/local/bin`; do man $i > "$i".md; done
